d944061f5cd7b39759f0d7cd491c1083562c42e3
fab <fabrizio.salmi@gmail.com>
2023-09-09 16:43:59 +0200

    Update update_rpz_blacklist.sh

--- scripts/update_rpz_blacklist.sh
+++ scripts/update_rpz_blacklist.sh
@@ -1,43 +1,59 @@
 #!/bin/bash
 
-# ----------------------
-# INSTRUCTIONS
-# ----------------------
-# 1. Update the DIRECTORY variable to where you want to store the downloaded blacklist.
-# 2. Make this script executable: chmod +x update_rpz_blacklist.sh
-# 3. Optionally, schedule this script using cron to run at desired intervals.
-#    For daily execution at 2 AM:
-#    crontab -e
-#    Add: 0 2 * * * /path/to/script/update_rpz_blacklist.sh
-# 4. After running this script, BIND will be reloaded to apply the new RPZ blacklist.
-# ----------------------
+# ==============================
+# RPZ BLACKLIST UPDATER SCRIPT
+# ==============================
 
-# Directory to store the downloaded blacklist
-DIRECTORY="/path/to/store/rpz_blacklist"
+# Required commands
+REQUIRED_COMMANDS=("wget" "tar" "systemctl" "grep" "mkdir" "cat" "date" "named-checkconf")
 
-# URL to the RPZ blacklist
-BLACKLIST_URL="https://github.com/fabriziosalmi/blacklists/raw/main/rpz_blacklist.tar.gz"
+# Check if required commands exist
+for cmd in "${REQUIRED_COMMANDS[@]}"; do
+  command -v $cmd >/dev/null 2>&1 || { echo >&2 "Error: $cmd is required but it's not installed. Exiting."; exit 1; }
+done
+
+# Directory to store the RPZ blacklist
+RPZ_DIRECTORY="/path/to/store/rpz_blacklist"
+# URL of the RPZ blacklist
+RPZ_URL="https://github.com/fabriziosalmi/blacklists/raw/main/rpz_blacklist.tar.gz"
+# BIND configuration file
+BIND_CONFIG="/etc/bind/named.conf.local"
 
 # Ensure the directory exists
-mkdir -p $DIRECTORY
+mkdir -p $RPZ_DIRECTORY
 
-# Download and extract the RPZ blacklist
-wget -O $DIRECTORY/rpz_blacklist.tar.gz "$BLACKLIST_URL"
-tar -xzf $DIRECTORY/rpz_blacklist.tar.gz -C $DIRECTORY
+# Download the latest RPZ blacklist from the repository
+wget -O $RPZ_DIRECTORY/rpz_blacklist.tar.gz "$RPZ_URL"
 
-# Update BIND configuration if not already updated
-if ! grep -q "rpz.blacklist" /etc/bind/named.conf.local; then
-    cat <<EOL >> /etc/bind/named.conf.local
-zone "rpz.blacklist" {
-    type master;
-    file "$DIRECTORY/rpz_blacklist.txt";
-};
+# Extract the blacklist
+tar -xzf $RPZ_DIRECTORY/rpz_blacklist.tar.gz -C $RPZ_DIRECTORY
 
-options {
-    response-policy { zone "rpz.blacklist"; };
-};
-EOL
+# Check if the configuration is already added to avoid duplicate entries
+if ! grep -q "rpz.blacklist" $BIND_CONFIG; then
+    # Append configuration to BIND's config file
+    echo "zone \"rpz.blacklist\" {
+        type master;
+        file \"$RPZ_DIRECTORY/rpz_blacklist.txt\";
+    };" >> $BIND_CONFIG
+
+    echo "options {
+        response-policy { zone \"rpz.blacklist\"; };
+    };" >> $BIND_CONFIG
 fi
 
-# Reload BIND
-sudo systemctl reload bind9
+# Check BIND configuration
+named-checkconf $BIND_CONFIG
+if [ $? -ne 0 ]; then
+    echo "Error in BIND configuration. Please check manually!"
+    exit 1
+fi
+
+echo "Script executed successfully!"
+
+# You can manually reload BIND to apply the new blacklist with: 
+# sudo systemctl reload bind9
+# Schedule this script using cron to automate the process. 
+# For example, to run it daily at 2 AM:
+# crontab -e
+# Add:
+# 0 2 * * * /path/to/this_script/update_rpz_blacklist.sh
