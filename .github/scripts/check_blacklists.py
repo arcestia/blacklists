8df8c5264811beb49a5aefd0f81ca1e281939614
fab <fabrizio.salmi@gmail.com>
2023-09-11 09:47:44 +0200

    Update check_blacklists.py

--- .github/scripts/check_blacklists.py
+++ .github/scripts/check_blacklists.py
@@ -6,20 +6,25 @@ from datetime import datetime
 # File paths
 BLACKLISTS_URL_FILE = 'blacklists.fqdn.urls'
 BLACKLIST_MONITOR_MD = 'blacklists_monitor.md'
-TEMP_DOWNLOAD_PATH = 'temp_download_file'
+GITHUB_API_URL = "https://api.github.com/repos/{owner}/{repo}/commits?path={path}"
 
 def get_last_modified(url):
     try:
         if 'raw.githubusercontent.com' in url:
-            # Download the file temporarily
-            with requests.get(url, stream=True) as r:
-                r.raise_for_status()
-                with open(TEMP_DOWNLOAD_PATH, 'wb') as f:
-                    for chunk in r.iter_content(chunk_size=8192):
-                        f.write(chunk)
-            # Check the file's modified time
-            file_time = os.path.getmtime(TEMP_DOWNLOAD_PATH)
-            return datetime.utcfromtimestamp(file_time).strftime('%Y-%m-%d %H:%M:%S UTC')
+            # Parsing the URL to retrieve repository details
+            parts = url.split("/")
+            owner = parts[3]
+            repo = parts[4]
+            path = "/".join(parts[5:])
+            api_url = GITHUB_API_URL.format(owner=owner, repo=repo, path=path)
+
+            response = requests.get(api_url)
+            response.raise_for_status()
+            data = response.json()
+            if data and 'commit' in data[0] and 'committer' in data[0]['commit']:
+                return data[0]['commit']['committer']['date']
+            else:
+                return "N/A"
         else:
             response = requests.head(url, timeout=10)
             if response.status_code == 200 and 'Last-Modified' in response.headers:
@@ -27,19 +32,15 @@ def get_last_modified(url):
             return "N/A"
     except requests.RequestException:
         return "Error"
-    finally:
-        # Cleanup the temporary file if it exists
-        if os.path.exists(TEMP_DOWNLOAD_PATH):
-            os.remove(TEMP_DOWNLOAD_PATH)
 
 def main():
     with open(BLACKLISTS_URL_FILE, 'r') as f:
         urls = [line.strip() for line in f]
 
     table_data = []
-    
+
     print("Checking blacklist URLs...")
-    for url in tqdm(urls, desc="Progress", ncols=100):
+    for url in urls:
         last_modified = get_last_modified(url)
         table_data.append((url, last_modified))
 
